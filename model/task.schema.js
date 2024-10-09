c201ded3b5dcbc27db40edfdf3dbcd5dd86b01dd
Jaydip <jaydipgadhiya0369@gmail.com>
2024-10-09 09:20:54 +0530

    task.schema.js

--- model/task.schema.js
+++ model/task.schema.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const task = new mongoose.Schema({
     taskName: String, 
     description: String,
-    status: String, enum: ["pending", "in-progress", "completed"], default: "pending"
+    status: String, enum: ["pending", "in-progress", "completed"]
 })
 
 const Task = mongoose.model('Task', task)
