1b889fa2b4ac76fef0dfb12d9c3e478f4ba56022
Jaydip <jaydipgadhiya0369@gmail.com>
2024-10-14 09:24:11 +0530

    index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require ('express')
 const db = require('./config/db')
 const cors = require('cors')
+const userRoute = require('./routes/user.route')
+const taskRoute = require('./routes/task.route')
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-
-
+app.use(express.urlencoded({extended:true}))
+app.use("/user", userRoute)
+app.use("/task", taskRoute)
 
 app.listen(8090, ()=> {
     console.log('listing on port 8090');
